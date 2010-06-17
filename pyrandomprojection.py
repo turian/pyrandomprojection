e7276546a2da7619a43950cf10564e40cdbacc2b
Joseph Turian <turian@gmail.com>
2010-06-17 14:21:05 -0400

    Began skeleton code

---
+++ pyrandomprojection.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+Random projection library for Python, converting a dictionary to
+low-dimensional numpy matrix
+"""
+
+def project(dict):
+    return dict
+
+
+if __name__ == "__main__":
+    dict = {"cool": 10, "boy": 20}
+    print project(dict)
