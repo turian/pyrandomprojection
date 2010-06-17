e5cf28290e7e47983f8f121b2eee28fb428edd96
Joseph Turian <turian@gmail.com>
2010-06-17 17:57:23 -0400

    Code now works?

--- pyrandomprojection.py
+++ pyrandomprojection.py
@@ -4,9 +4,11 @@ Random projection library for Python, converting a dictionary to
 low-dimensional numpy vector
 """
 
-import math
-import murmur
+#import math
+#import murmur
 import numpy
+from common.deterministicrandom import deterministicrandom
+import common.gaussian
 
 def project(dict, dimensions, RANDOMIZATION_TYPE="gaussian", TERNARY_NON_ZERO_PERCENT=None, RANDOM_SEED=0):
     """
@@ -19,7 +21,7 @@ def project(dict, dimensions, RANDOMIZATION_TYPE="gaussian", TERNARY_NON_ZERO_PE
     for key, value in dict.items():
         projection += value * randomrow(key, dimensions, RANDOMIZATION_TYPE, TERNARY_NON_ZERO_PERCENT, RANDOM_SEED)
 
-    return dict
+    return projection
 
 def randomrow(key, dimensions, RANDOMIZATION_TYPE="gaussian", TERNARY_NON_ZERO_PERCENT=None, RANDOM_SEED=0):
     """
@@ -30,32 +32,21 @@ def randomrow(key, dimensions, RANDOMIZATION_TYPE="gaussian", TERNARY_NON_ZERO_P
     TERNARY_NON_ZERO_PERCENT: float in range (0.0, 1.0), percent of elements in random matrix that are non-zero.
     """
     if RANDOMIZATION_TYPE == "gaussian":
-        foo = []
+        values = []
         for i in range(dimensions):
             # Unique name for this matrix entry
             matrix_entry_name = `key` + "-" + `i` + "-" + `RANDOM_SEED`
-#            theta=urand(0,2*PI);
-            rsq=genexp(0.5);
-            x=sqrt(rsq)*cos(theta);
-            return(x);
 
-#            gaussian_value = 1 / math.sqrt(2 * math.pi) * math.exp(-0.5 * (long_value**2))
+            matrix_entry_name_random = deterministicrandom(matrix_entry_name)
+            gaussian_value = common.gaussian.from_probability(matrix_entry_name_random)
 
-            import scipy.stats.distributions
-
-#            scipy.stats.distributions.normal.pdf(x)
-
-            print scipy.stats.norm.pdf(long_value)
-#            norm = norm_gen(name = 'norm', longname = 'A normal', extradoc = """
-
-#            from scipy.special import erf
-#            gaussian_value = 0.5 + 0.5 * erf(long_value/sqrt(
-
-#            print long_value
-            foo.append(gaussian_value)
-        print numpy.mean(foo)
-        print numpy.var(foo)
-        print numpy.std(foo)
+            values.append(gaussian_value)
+        numpy_values = numpy.array(values)
+        assert numpy_values.shape == (dimensions,)
+#        print numpy.mean(foo)
+#        print numpy.var(foo)
+#        print numpy.std(foo)
+        return numpy_values
     elif RANDOMIZATION_TYPE == "ternary":
         assert TERNARY_NON_ZERO_PERCENT > 0.0 and TERNARY_NON_ZERO_PERCENT < 1.0
         assert 0        # Not yet implemented
